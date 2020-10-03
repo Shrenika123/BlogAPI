71b7afdde5bcde7494a6f4f14ba9ea7b0a0080d5
“Shrenika123” <shrenika.nataraj@gmail.com>
2020-10-03 11:16:18 +0530

    first commit

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const bodyparser=require('body-parser')
 const app=express()
 const port=process.env.PORT||9000
 const school=require('./1.json')
+const data=require('./data')
+
 school.pop()
 school.shift()
 console.log(school)
@@ -12,6 +14,10 @@ app.use(bodyparser.json())
 
 app.get('/schooldetails',(req,res)=>{
     res.status(200).send(school)
+})
+
+app.get('/products',(req,res)=>{
+    res.status(200).send(data)
 })
 
 app.listen(port,()=>{
