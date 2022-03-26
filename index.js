06a57cee725d1976f37f22ae64d23a7d5054e1f7
“Shrenika123” <shrenika.nataraj@gmail.com>
2022-03-26 07:53:12 +0530

    blog api

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
-const express=require('express')
-const cors=require('cors')
-const bodyparser=require('body-parser')
-const app=express()
-const port=process.env.PORT||9000
-const school=require('./1.json')
-const data=require('./data')
+const express = require("express");
+const cors = require("cors");
+const bodyparser = require("body-parser");
+const app = express();
+const port = process.env.PORT || 9000;
+const blogs = require("./blogs.json");
+const comment = require("./data");
 
-school.pop()
-school.shift()
-console.log(school)
-app.use(cors())
-app.use(bodyparser.json())
+app.use(cors());
+app.use(bodyparser.json());
 
-app.get('/schooldetails',(req,res)=>{
-    res.status(200).send(school)
-})
+app.get("/getBlogs", (req, res) => {
+  res.status(200).send(blogs);
+});
 
-app.get('/products',(req,res)=>{
-    res.status(200).send(data)
-})
+app.get("/getComments", (req, res) => {
+  res.status(200).send(comment);
+});
 
-app.listen(port,()=>{
-    console.log(`listening on port ${port}`)
-})
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
